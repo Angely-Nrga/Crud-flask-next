07c7d427f363222e4682faa0f9365a50983d1edd
Angely-Nrga <angelynoeriega@gmail.com>
2026-06-09 13:48:38 -0500

    force render update

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+CORS(app)
 
 # 🗄️ BASE DE DATOS SQLITE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
